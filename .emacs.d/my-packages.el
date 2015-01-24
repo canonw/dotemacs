82c8cdc016fccd56ab0b017ee187d43795ad6992
unknown <canonw@users.noreply.github.com>
2015-01-24 09:06:34 -0800

    Enable package download.

--- .emacs.d/my-packages.el
+++ .emacs.d/my-packages.el
@@ -1,5 +1,5 @@
 ; ~/.emacs.d/my-packages.el
-; (require 'cl)
+(require 'cl)
 
 (when (>= emacs-major-version 24)
   (require 'package)
@@ -10,25 +10,28 @@
   (package-initialize)
   )
 
-; (defvar required-packages
-;   '(
+(defvar required-packages
+  '(
+    evil
+    solarized-theme
+;    csharp-mode
 ;     magit
 ;     yasnippet
-;   ) "a list of packages to ensure are installed at launch.")
-; 
-; ; method to check if all packages are installed
-; (defun packages-installed-p ()
-;   (loop for p in required-packages
-;         when (not (package-installed-p p)) do (return nil)
-;         finally (return t)))
-; 
-; ; if not all packages are installed, check one by one and install the missing ones.
-; (unless (packages-installed-p)
-;   ; check for new packages (package versions)
-;   (message "%s" "Emacs is now refreshing its package database...")
-;   (package-refresh-contents)
-;   (message "%s" " done.")
-;   ; install the missing packages
-;   (dolist (p required-packages)
-;     (when (not (package-installed-p p))
-;       (package-install p))))
+   ) "a list of packages to ensure are installed at launch.")
+
+; method to check if all packages are installed
+(defun packages-installed-p ()
+  (loop for p in required-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+; if not all packages are installed, check one by one and in the missing ones.
+(unless (packages-installed-p)
+  ; check for new packages (package versions)
+  (message "%s" "Emacs is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ; install the missing packages
+  (dolist (p required-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
