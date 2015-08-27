eb54cd5cbfdd5a98e0428efef1e260c2603a7f60
Ken Wong <canonw@users.noreply.github.com>
2015-08-26 18:08:28 -0700

    Add files to ignore

--- .emacs.d/lisp/init-recentf.el
+++ .emacs.d/lisp/init-recentf.el
@@ -3,6 +3,9 @@
 (setq recentf-max-saved-items 1000
       recentf-exclude '("/tmp/" "/ssh:"))
 
+;; Ignore file list
+(add-to-list 'recentf-exclude "\\ido.last\\'")
+
 (global-set-key "\C-cr" 'recentf-open-files)
 
 (provide 'init-recentf)
