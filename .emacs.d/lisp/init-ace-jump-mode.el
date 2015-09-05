801d781df221a19b5339f969fc37fcb1858cf0a8
Ken Wong <ken.yui.wong@gmail.com>
2015-09-05 15:31:38 -0700

    Move key binding in ace-jump-mode.

--- .emacs.d/lisp/init-ace-jump-mode.el
+++ .emacs.d/lisp/init-ace-jump-mode.el
@@ -4,8 +4,6 @@
   "ace-jump-mode"
   "Emacs quick move minor mode"
   t)
-;; you can select the key you prefer to
-(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
 
 ;; enable a more powerful jump back function from ace jump mode
 ;;
@@ -17,8 +15,5 @@
 ;;(eval-after-load "ace-jump-mode"
 ;;                   '(ace-jump-mode-enable-mark-sync))
 ;;(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
-
-;;If you use evil
-(eval-after-load "evil" '(define-key evil-normal-state-map (kbd "SPC") 'ace-jump-mode))
 
 (provide 'init-ace-jump-mode)
--- .emacs.d/lisp/init-key-bindings.el
+++ .emacs.d/lisp/init-key-bindings.el
@@ -1,3 +1,10 @@
 ;; Centralize all libraries in one place
 
+;;
+;; ace-jump-mode
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+ 
+;;If you use evil
+; (eval-after-load "evil" '(define-key evil-normal-state-map (kbd "SPC") 'ace-jump-mode))
+
 (provide 'init-key-bindings)
