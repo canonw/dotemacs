9b050616be3e0e02e864e925022de1252995e507
Ken Wong <canonw@users.noreply.github.com>
2016-03-26 18:46:09 -0700

    Update key binding

--- .emacs.d/lisp/init-key-bindings.el
+++ .emacs.d/lisp/init-key-bindings.el
@@ -331,6 +331,7 @@
 (global-set-key (kbd "M-x")                          'helm-M-x)
 (global-set-key (kbd "M-y")                          'helm-show-kill-ring)
 (global-set-key (kbd "C-c f")                        'helm-recentf)
+(global-set-key (kbd "C-x b")                        'helm-buffers-list)
 (global-set-key (kbd "C-x C-f")                      'helm-find-files)
 ;;(global-set-key (kbd "C-c <SPC>")                    'helm-all-mark-rings)
 (global-set-key (kbd "C-x r b")                      'helm-filtered-bookmarks)
