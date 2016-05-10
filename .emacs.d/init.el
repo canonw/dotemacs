4e14981e01fbfd8012c6321bfdd72c299df6673b
Ken Wong <canonw@users.noreply.github.com>
2016-05-10 08:54:33 -0700

    Update golden ratio setting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -84,8 +84,8 @@
   (defun pl/helm-alive-p ()
     (if (boundp 'helm-alive-p)
         (symbol-value 'helm-alive-p)))
-
-(add-to-list 'golden-ratio-inhibit-functions 'pl/helm-alive-p)
+  (add-to-list 'golden-ratio-inhibit-functions 'pl/helm-alive-p)
+  (add-to-list 'golden-ratio-exclude-modes 'calendar-mode)
   )
 
 (use-package flyspell
