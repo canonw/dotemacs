9a48e03b4d9508cc3d8ac84db12d839087a10f18
Ken Wong <canonw@users.noreply.github.com>
2017-03-01 12:22:54 -0800

    Add init-alias.el

---
+++ .emacs.d/lisp/init-alias.el
@@ -0,0 +1,11 @@
+; http://ergoemacs.org/emacs/emacs_alias.html
+
+; major modes
+(defalias 'hm 'html-mode)
+(defalias 'tm 'text-mode)
+(defalias 'slm 'sql-mode)
+(defalias 'elm 'emacs-lisp-mode)
+(defalias 'om 'org-mode)
+(defalias 'ssm 'shell-script-mode)
+
+(provide 'init-alias)
