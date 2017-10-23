fbe1da0eb5bfb9d896cbc9da42b5fb3fda3c97d1
Ken Wong <canonw@users.noreply.github.com>
2017-10-23 19:28:12 +0000

    Move code to init-java-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -231,37 +231,7 @@
   :mode ("\\.py\\'" . python-mode)
   :interpreter ("python" . python-mode))
 
-(use-package eclim
-  :init
-  (progn
-    (use-package eclimd)
-    (add-hook 'java-mode-hook (lambda () (eclim-mode 1)))
-
-    (add-hook 'eclim-mode-hook 'setup-eclim)
-    (defun setup-eclim ()
-      (progn
-        (setq c-basic-offset 4)))
-
-    (setq eclim-executable "~/.local/share/umake/ide/eclipse/eclim")
-    (setq eclim-auto-save nil)
-    (setq eclim-eclipse-dirs '("~/.local/share/umake/ide/eclipse"))
-    (setq eclimd-default-workspace "~/eclim-workspace")
-
-    (global-eclim-mode)
-    (use-package eclimd)
-    (use-package company-emacs-eclim
-      :config
-      (company-emacs-eclim-setup)
-      )
-    ;; temporarily switch to emacs mode for this eclim command (with evil mode)
-    ;; (defadvice eclim-java-import-organize (around temporary-emacs-mode)
-    ;;   (let ((evil-emacs-state))
-    ;;     ad-do-it))
-    ;; (ad-activate 'eclim-java-import-organize)
-
-    )
-  )
-
+(require 'init-java-mode)
 
 (use-package yaml-mode
   :defer t
@@ -410,4 +380,3 @@ See help of `format-time-string' for possible replacements")
     (add-to-list 'exec-path "C:/ProgramData/chocolatey/bin")
     (setenv "PATH" (mapconcat #'identity exec-path path-separator))
     ) 
-
